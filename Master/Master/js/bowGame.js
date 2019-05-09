1f56e31e29eec85a07ca21af55a1d707ecd5875a
jordandelaney2018 <35698897+jordandelaney2018@users.noreply.github.com>
2019-05-09 19:26:52 +0100

    Add files via upload

--- Master/Master/js/bowGame.js
+++ Master/Master/js/bowGame.js
@@ -386,6 +386,123 @@ function ease(t) {
     return (Math.sin(t * Math.PI - Math.PI / 2) + 1) / 2;
 }
 
+
+// init json array
+var jsonFrm = 0;
+var jsonMotion = null;
+
+//usage:
+readTextFile("archermotion.json", function(text){
+    jsonMotion = JSON.parse(text);
+    var count = Object.keys(jsonMotion).length;
+    //console.log(count);
+    jsonFrm = count;
+    iFrame = 0;
+});
+
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {//} && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Kinectron codes starting from here///////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Initialize kinectron
+kinectron = new Kinectron("192.168.60.56"); // Define and create an instance of kinectron
+kinectron.makeConnection(); // Create connection between remote and application
+kinectron.startTrackedBodies(getBodies); // Start tracked bodies and set callback
+
+// The getBodies callback function: called once every time kinect obtain a frame
+function getBodies(skeleton)
+{
+    meshLH.position.x = skeleton.joints[kinectron.HANDLEFT].cameraX;
+    meshLH.position.y = skeleton.joints[kinectron.HANDLEFT].cameraY;
+    meshLH.position.z = skeleton.joints[kinectron.HANDLEFT].cameraZ;
+    meshRH.position.x = skeleton.joints[kinectron.HANDRIGHT].cameraX;
+    meshRH.position.y = skeleton.joints[kinectron.HANDRIGHT].cameraY;
+    meshRH.position.z = skeleton.joints[kinectron.HANDRIGHT].cameraZ;
+
+    head_msh.position.x = skeleton.joints[kinectron.HEAD].cameraX;
+    head_msh.position.y = skeleton.joints[kinectron.HEAD].cameraY;
+    head_msh.position.z = skeleton.joints[kinectron.HEAD].cameraZ;
+
+    //Left arm
+    //Shoulder
+    leftShoulder_msh.position.x = skeleton.joints[kinectron.SHOULDERLEFT].cameraX;
+    leftShoulder_msh.position.y = skeleton.joints[kinectron.SHOULDERLEFT].cameraY;
+    leftShoulder_msh.position.z = skeleton.joints[kinectron.SHOULDERLEFT].cameraZ;
+
+    LeftArm.geometry.vertices[0].x = skeleton.joints[kinectron.SPINESHOULDER].cameraX;
+    LeftArm.geometry.vertices[0].y = skeleton.joints[kinectron.SPINESHOULDER].cameraY;
+    LeftArm.geometry.vertices[0].z = skeleton.joints[kinectron.SPINESHOULDER].cameraZ;
+
+    LeftArm.geometry.vertices[1].x = skeleton.joints[kinectron.SHOULDERLEFT].cameraX;
+    LeftArm.geometry.vertices[1].y = skeleton.joints[kinectron.SHOULDERLEFT].cameraY;
+    LeftArm.geometry.vertices[1].z = skeleton.joints[kinectron.SHOULDERLEFT].cameraZ;
+
+    LeftArm.geometry.vertices[2].x = skeleton.joints[kinectron.ELBOWLEFT].cameraX;
+    LeftArm.geometry.vertices[2].y = skeleton.joints[kinectron.ELBOWLEFT].cameraY;
+    LeftArm.geometry.vertices[2].z = skeleton.joints[kinectron.ELBOWLEFT].cameraZ;
+
+    LeftArm.geometry.vertices[3].x = skeleton.joints[kinectron.HANDLEFT].cameraX;
+    LeftArm.geometry.vertices[3].y = skeleton.joints[kinectron.HANDLEFT].cameraY;
+    LeftArm.geometry.vertices[3].z = skeleton.joints[kinectron.HANDLEFT].cameraZ;
+    LeftArm.geometry.verticesNeedUpdate = true;
+
+    //Right arm
+    //Shoulder
+    rightShoulder_msh.position.x = skeleton.joints[kinectron.SHOULDERRIGHT].cameraX;
+    rightShoulder_msh.position.y = skeleton.joints[kinectron.SHOULDERRIGHT].cameraY;
+    rightShoulder_msh.position.z = skeleton.joints[kinectron.SHOULDERRIGHT].cameraZ;
+
+    RightArm.geometry.vertices[0].x = skeleton.joints[kinectron.SPINESHOULDER].cameraX;
+    RightArm.geometry.vertices[0].y = skeleton.joints[kinectron.SPINESHOULDER].cameraY;
+    RightArm.geometry.vertices[0].z = skeleton.joints[kinectron.SPINESHOULDER].cameraZ;
+
+    RightArm.geometry.vertices[1].x = skeleton.joints[kinectron.SHOULDERRIGHT].cameraX;
+    RightArm.geometry.vertices[1].y = skeleton.joints[kinectron.SHOULDERRIGHT].cameraY;
+    RightArm.geometry.vertices[1].z = skeleton.joints[kinectron.SHOULDERRIGHT].cameraZ;
+
+    RightArm.geometry.vertices[2].x = skeleton.joints[kinectron.ELBOWRIGHT].cameraX;
+    RightArm.geometry.vertices[2].y = skeleton.joints[kinectron.ELBOWRIGHT].cameraY;
+    RightArm.geometry.vertices[2].z = skeleton.joints[kinectron.ELBOWRIGHT].cameraZ;
+
+    RightArm.geometry.vertices[3].x = skeleton.joints[kinectron.HANDRIGHT].cameraX;
+    RightArm.geometry.vertices[3].y = skeleton.joints[kinectron.HANDRIGHT].cameraY;
+    RightArm.geometry.vertices[3].z = skeleton.joints[kinectron.HANDRIGHT].cameraZ;
+    RightArm.geometry.verticesNeedUpdate = true;
+
+    //Spine
+    Spine.geometry.vertices[0].x = skeleton.joints[kinectron.HEAD].cameraX;
+    Spine.geometry.vertices[0].y = skeleton.joints[kinectron.HEAD].cameraY;
+    Spine.geometry.vertices[0].z = skeleton.joints[kinectron.HEAD].cameraZ;
+
+    Spine.geometry.vertices[1].x = skeleton.joints[kinectron.NECK].cameraX;
+    Spine.geometry.vertices[1].y = skeleton.joints[kinectron.NECK].cameraY;
+    Spine.geometry.vertices[1].z = skeleton.joints[kinectron.NECK].cameraZ;
+
+    Spine.geometry.vertices[2].x = skeleton.joints[kinectron.SPINEMID].cameraX;
+    Spine.geometry.vertices[2].y = skeleton.joints[kinectron.SPINEMID].cameraY;
+    Spine.geometry.vertices[2].z = skeleton.joints[kinectron.SPINEMID].cameraZ;
+
+    Spine.geometry.vertices[3].x = skeleton.joints[kinectron.SPINEBASE].cameraX;
+    Spine.geometry.vertices[3].y = skeleton.joints[kinectron.SPINEBASE].cameraY;
+    Spine.geometry.vertices[3].z = skeleton.joints[kinectron.SPINEBASE].cameraZ;
+    Spine.geometry.verticesNeedUpdate = true;
+
+    pelvis_msh.position.x = skeleton.joints[kinectron.SPINEBASE].cameraX;
+    pelvis_msh.position.y = skeleton.joints[kinectron.SPINEBASE].cameraY;
+    pelvis_msh.position.z = skeleton.joints[kinectron.SPINEBASE].cameraZ;
+}
 var score =0;
 var iFrame = 0;
 var arrows = []; //0 = latest arrow
@@ -555,148 +672,3 @@ function animate() {
 }
 
 animate();
-// init json array
-var jsonFrm = 0;
-var jsonMotion = null;
-
-//usage:
-readTextFile("archermotion.json", function(text){
-    jsonMotion = JSON.parse(text);
-    var count = Object.keys(jsonMotion).length;
-    //console.log(count);
-    jsonFrm = count;
-    iFrame = 0;
-});
-
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4) {//} && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Kinectron codes starting from here///////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Initialize kinectron
-kinectron = new Kinectron("192.168.60.56"); // Define and create an instance of kinectron
-kinectron.makeConnection(); // Create connection between remote and application
-kinectron.startTrackedBodies(getBodies); // Start tracked bodies and set callback
-
-// The getBodies callback function: called once every time kinect obtain a frame
-function getBodies(skeleton)
-{
-    meshLH.position.x = skeleton.joints[kinectron.HANDLEFT].cameraX;
-    meshLH.position.y = skeleton.joints[kinectron.HANDLEFT].cameraY;
-    meshLH.position.z = skeleton.joints[kinectron.HANDLEFT].cameraZ;
-    meshRH.position.x = skeleton.joints[kinectron.HANDRIGHT].cameraX;
-    meshRH.position.y = skeleton.joints[kinectron.HANDRIGHT].cameraY;
-    meshRH.position.z = skeleton.joints[kinectron.HANDRIGHT].cameraZ;
-
-    head_msh.position.x = skeleton.joints[kinectron.HEAD].cameraX;
-    head_msh.position.y = skeleton.joints[kinectron.HEAD].cameraY;
-    head_msh.position.z = skeleton.joints[kinectron.HEAD].cameraZ;
-
-    //Left arm
-    //Shoulder
-    leftShoulder_msh.position.x = skeleton.joints[kinectron.SHOULDERLEFT].cameraX;
-    leftShoulder_msh.position.y = skeleton.joints[kinectron.SHOULDERLEFT].cameraY;
-    leftShoulder_msh.position.z = skeleton.joints[kinectron.SHOULDERLEFT].cameraZ;
-
-    LeftArm.geometry.vertices[0].x = skeleton.joints[kinectron.SPINESHOULDER].cameraX;
-    LeftArm.geometry.vertices[0].y = skeleton.joints[kinectron.SPINESHOULDER].cameraY;
-    LeftArm.geometry.vertices[0].z = skeleton.joints[kinectron.SPINESHOULDER].cameraZ;
-
-    LeftArm.geometry.vertices[1].x = skeleton.joints[kinectron.SHOULDERLEFT].cameraX;
-    LeftArm.geometry.vertices[1].y = skeleton.joints[kinectron.SHOULDERLEFT].cameraY;
-    LeftArm.geometry.vertices[1].z = skeleton.joints[kinectron.SHOULDERLEFT].cameraZ;
-
-    LeftArm.geometry.vertices[2].x = skeleton.joints[kinectron.ELBOWLEFT].cameraX;
-    LeftArm.geometry.vertices[2].y = skeleton.joints[kinectron.ELBOWLEFT].cameraY;
-    LeftArm.geometry.vertices[2].z = skeleton.joints[kinectron.ELBOWLEFT].cameraZ;
-
-    LeftArm.geometry.vertices[3].x = skeleton.joints[kinectron.HANDLEFT].cameraX;
-    LeftArm.geometry.vertices[3].y = skeleton.joints[kinectron.HANDLEFT].cameraY;
-    LeftArm.geometry.vertices[3].z = skeleton.joints[kinectron.HANDLEFT].cameraZ;
-    LeftArm.geometry.verticesNeedUpdate = true;
-
-    //Right arm
-    //Shoulder
-    rightShoulder_msh.position.x = skeleton.joints[kinectron.SHOULDERRIGHT].cameraX;
-    rightShoulder_msh.position.y = skeleton.joints[kinectron.SHOULDERRIGHT].cameraY;
-    rightShoulder_msh.position.z = skeleton.joints[kinectron.SHOULDERRIGHT].cameraZ;
-
-    RightArm.geometry.vertices[0].x = skeleton.joints[kinectron.SPINESHOULDER].cameraX;
-    RightArm.geometry.vertices[0].y = skeleton.joints[kinectron.SPINESHOULDER].cameraY;
-    RightArm.geometry.vertices[0].z = skeleton.joints[kinectron.SPINESHOULDER].cameraZ;
-
-    RightArm.geometry.vertices[1].x = skeleton.joints[kinectron.SHOULDERRIGHT].cameraX;
-    RightArm.geometry.vertices[1].y = skeleton.joints[kinectron.SHOULDERRIGHT].cameraY;
-    RightArm.geometry.vertices[1].z = skeleton.joints[kinectron.SHOULDERRIGHT].cameraZ;
-
-    RightArm.geometry.vertices[2].x = skeleton.joints[kinectron.ELBOWRIGHT].cameraX;
-    RightArm.geometry.vertices[2].y = skeleton.joints[kinectron.ELBOWRIGHT].cameraY;
-    RightArm.geometry.vertices[2].z = skeleton.joints[kinectron.ELBOWRIGHT].cameraZ;
-
-    RightArm.geometry.vertices[3].x = skeleton.joints[kinectron.HANDRIGHT].cameraX;
-    RightArm.geometry.vertices[3].y = skeleton.joints[kinectron.HANDRIGHT].cameraY;
-    RightArm.geometry.vertices[3].z = skeleton.joints[kinectron.HANDRIGHT].cameraZ;
-    RightArm.geometry.verticesNeedUpdate = true;
-
-    //Spine
-    Spine.geometry.vertices[0].x = skeleton.joints[kinectron.HEAD].cameraX;
-    Spine.geometry.vertices[0].y = skeleton.joints[kinectron.HEAD].cameraY;
-    Spine.geometry.vertices[0].z = skeleton.joints[kinectron.HEAD].cameraZ;
-
-    Spine.geometry.vertices[1].x = skeleton.joints[kinectron.NECK].cameraX;
-    Spine.geometry.vertices[1].y = skeleton.joints[kinectron.NECK].cameraY;
-    Spine.geometry.vertices[1].z = skeleton.joints[kinectron.NECK].cameraZ;
-
-    Spine.geometry.vertices[2].x = skeleton.joints[kinectron.SPINEMID].cameraX;
-    Spine.geometry.vertices[2].y = skeleton.joints[kinectron.SPINEMID].cameraY;
-    Spine.geometry.vertices[2].z = skeleton.joints[kinectron.SPINEMID].cameraZ;
-
-    Spine.geometry.vertices[3].x = skeleton.joints[kinectron.SPINEBASE].cameraX;
-    Spine.geometry.vertices[3].y = skeleton.joints[kinectron.SPINEBASE].cameraY;
-    Spine.geometry.vertices[3].z = skeleton.joints[kinectron.SPINEBASE].cameraZ;
-    Spine.geometry.verticesNeedUpdate = true;
-
-    pelvis_msh.position.x = skeleton.joints[kinectron.SPINEBASE].cameraX;
-    pelvis_msh.position.y = skeleton.joints[kinectron.SPINEBASE].cameraY;
-    pelvis_msh.position.z = skeleton.joints[kinectron.SPINEBASE].cameraZ;
-
-    //Left Leg
-    /*LeftLeg.geometry.vertices[0].x = skeleton.joints[kinectron.HIPLEFT].cameraX;
-	LeftLeg.geometry.vertices[0].y = skeleton.joints[kinectron.HIPLEFT].cameraY;
-	LeftLeg.geometry.vertices[0].z = skeleton.joints[kinectron.HIPLEFT].cameraZ;
-
-	LeftLeg.geometry.vertices[1].x = skeleton.joints[kinectron.KNEELEFT].cameraX;
-	LeftLeg.geometry.vertices[1].y = skeleton.joints[kinectron.KNEELEFT].cameraY;
-	LeftLeg.geometry.vertices[1].z = skeleton.joints[kinectron.KNEELEFT].cameraZ;
-
-	LeftLeg.geometry.vertices[2].x = skeleton.joints[kinectron.ANKLELEFT].cameraX;
-	LeftLeg.geometry.vertices[2].y = skeleton.joints[kinectron.ANKLELEFT].cameraY;
-	LeftLeg.geometry.vertices[2].z = skeleton.joints[kinectron.ANKLELEFT].cameraZ;
-	LeftLeg.geometry.verticesNeedUpdate = true;
-
-	//Right Leg
-	RightLeg.geometry.vertices[0].x = skeleton.joints[kinectron.HIPRIGHT].cameraX;
-	RightLeg.geometry.vertices[0].y = skeleton.joints[kinectron.HIPRIGHT].cameraY;
-	RightLeg.geometry.vertices[0].z = skeleton.joints[kinectron.HIPRIGHT].cameraZ;
-
-	RightLeg.geometry.vertices[1].x = skeleton.joints[kinectron.KNEERIGHT].cameraX;
-	RightLeg.geometry.vertices[1].y = skeleton.joints[kinectron.KNEERIGHT].cameraY;
-	RightLeg.geometry.vertices[1].z = skeleton.joints[kinectron.KNEERIGHT].cameraZ;
-
-	RightLeg.geometry.vertices[2].x = skeleton.joints[kinectron.ANKLERIGHT].cameraX;
-	RightLeg.geometry.vertices[2].y = skeleton.joints[kinectron.ANKLERIGHT].cameraY;
-	RightLeg.geometry.vertices[2].z = skeleton.joints[kinectron.ANKLERIGHT].cameraZ;
-	RightLeg.geometry.verticesNeedUpdate = true;*/
-}
-
